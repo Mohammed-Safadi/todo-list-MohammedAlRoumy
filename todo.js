2bb06ebf6412aafa79656dc8d36a41ad355b81be
Mohammed Alroumy <mohamed.alroumy@gmail.com>
2020-04-14 10:46:37 +0300

    fixed js code

--- todo.js
+++ todo.js
@@ -1,31 +1,41 @@
-var taskInput = document.getElementById("new-task");//Add a new task.
-var addButton = document.getElementsByTagName("button")[0];//first button
-var tasks = document.getElementById("tasks");//ul of tasks
+var taskInput = document.getElementById("new-task"); //Add a new task.
+var addButton = document.getElementsByTagName("button")[0]; //first button
+var tasks = document.getElementById("tasks"); //ul of tasks
 
+tasks.addEventListener("click", function (e) {
+    console.log(e.target.className == "done");
+    if (e.target.className == "done") {
+        changeColor(e.target);
+    }
+
+    if (e.target.className == "delete") {
+        deleteTask(e.target);
+    }
+
+    e.preventDefault();
+});
 
 //New task list item
 var createNewTaskElement = function (taskString) {
-
     var listItem = document.createElement("li");
 
-
     //label
-    var label = document.createElement("label");//label
+    var label = document.createElement("label"); //label
 
     //button.done
-    var doneButton = document.createElement("button");//done button
+    var doneButton = document.createElement("button"); //done button
 
     //span
-    var span = document.createElement("span");//span
+    var span = document.createElement("span"); //span
 
     //span between buttons
-    var dash = document.createElement("span");//span
+    var dash = document.createElement("span"); //span
 
     // div btn
     var btn = document.createElement("div");
 
     //button.delete
-    var deleteButton = document.createElement("button");//delete button
+    var deleteButton = document.createElement("button"); //delete button
 
     label.innerText = taskString;
 
@@ -34,7 +44,7 @@ var createNewTaskElement = function (taskString) {
     doneButton.innerText = "Make as Done";
     doneButton.className = "done";
     deleteButton.innerText = "Delete";
-    dash.innerText = "|";
+    dash.innerText = "||";
     dash.className = "dash";
     deleteButton.className = "delete";
     btn.className = "btn";
@@ -47,11 +57,9 @@ var createNewTaskElement = function (taskString) {
     listItem.appendChild(label);
     listItem.appendChild(btn);
     return listItem;
-}
-
+};
 
 var addTask = function () {
-
     if (taskInput.value != "") {
         console.log("Add Task...");
 
@@ -62,45 +70,28 @@ var addTask = function () {
     } else {
         alert("empty task ... phase inter data");
     }
-
-}
+};
 
 //Set the click handler to the addTask function.
 addButton.onclick = addTask;
 
 //Delete task.
-var deleteTask = function () {
+var deleteTask = function (elemnt) {
     console.log("Delete Task...");
-    var btn = this.parentNode;
+    var btn = elemnt.parentNode;
     var listItem = btn.parentNode;
     var ul = listItem.parentNode;
 
     ul.removeChild(listItem);
-
-}
+};
 
 //change color task.
-var changeColor = function () {
+var changeColor = function (elemnt) {
     console.log("change color...");
-    var span = this.parentNode;
+    var span = elemnt.parentNode;
     var btn = span.parentNode;
     var listItem = btn.parentNode;
 
     listItem.style.color = "green";
     span.style.opacity = 0;
-}
-
-
-var bindTaskEvents = function (taskListItem) {
-    console.log("bind list item events");
-
-    var doneButton = taskListItem.querySelector("button.done");
-    var deleteButton = taskListItem.querySelector("button.delete");
-
-    deleteButton.onclick = deleteTask;
-    doneButton.onclick = changeColor;
-}
-
-for (var i = 0; i < tasks.children.length; i++) {
-    bindTaskEvents(tasks.children[i]);
-}
+};
